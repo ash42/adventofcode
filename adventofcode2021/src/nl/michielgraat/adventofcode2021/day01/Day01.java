f0ba924064f3fb8002dadebf0cbaf6d84d9fc7de
Michiel Graat <github@michielgraat.nl>
2021-12-02 09:56:14 +0100

    Start of AoC 2021

---
+++ adventofcode2021/src/nl/michielgraat/adventofcode2021/day01/Day01.java
@@ -0,0 +1,43 @@
+package nl.michielgraat.adventofcode2021.day01;
+
+import nl.michielgraat.adventofcode2021.FileReader;
+import java.util.Calendar;
+import java.util.List;
+
+public class Day01 {
+    private static final String FILENAME = "day01.txt";
+    
+    private int runPart2(List<Integer> lines) {
+        int total = 0;
+        for (int i=3; i<lines.size(); i++) {
+            int prev = lines.get(i-3) + lines.get(i-2) + lines.get(i-1);
+            int cur = lines.get(i-2) + lines.get(i-1) + lines.get(i);
+            if (prev < cur) {
+                total++;
+            }
+        }
+        return total;
+    }
+
+    private int runPart1(List<Integer> lines) {
+        int total = 0;
+        for (int i=1; i<lines.size(); i++) {
+            if (lines.get(i-1) < lines.get(i)) {
+                total++;
+            }
+        }
+        return total;
+    }
+
+    public static void main(String[] args) {
+		final List<Integer> lines = FileReader.getCompleteIntegerList(FILENAME);
+		long start = Calendar.getInstance().getTimeInMillis();
+		System.out.println("Answer to part 1: " + new Day01().runPart1(lines));
+		System.out.println("Took: " + (Calendar.getInstance().getTimeInMillis() - start) + " ms");
+		start = Calendar.getInstance().getTimeInMillis();
+		System.out.println("Answer to part 1: " + new Day01().runPart2(lines));
+		System.out.println("Took: " + (Calendar.getInstance().getTimeInMillis() - start) + " ms");
+
+	}
+
+}
---
+++ adventofcode2021/src/nl/michielgraat/adventofcode2021/day02/Day02.java
@@ -0,0 +1,55 @@
+package nl.michielgraat.adventofcode2021.day02;
+
+import nl.michielgraat.adventofcode2021.FileReader;
+import java.util.Calendar;
+import java.util.List;
+
+public class Day02 {
+    private static final String FILENAME = "day02.txt";
+    
+    private int runPart2(List<String> lines) {
+        int aim = 0;
+        int depth = 0;
+        int pos = 0;
+        for (String line : lines) {
+            int val = Integer.parseInt(line.substring(line.indexOf(" ") + 1));
+            if (line.startsWith("down")) {
+                aim += val;
+            } else if (line.startsWith("up")) {
+                aim -= val;
+            } else {
+                pos += val;
+                depth += aim*val;
+            }
+        }
+        return depth*pos;
+    }
+
+    private int runPart1(List<String> lines) {
+        int depth = 0;
+        int pos = 0;
+        for (String line : lines) {
+            int val = Integer.parseInt(line.substring(line.indexOf(" ") + 1));
+            if (line.startsWith("down")) {
+                depth += val;
+            } else if (line.startsWith("up")) {
+                depth -= val;
+            } else {
+                pos += val;
+            }
+        }
+        return depth*pos;
+    }
+
+    public static void main(String[] args) {
+		final List<String> lines = FileReader.getStringList(FILENAME);
+		long start = Calendar.getInstance().getTimeInMillis();
+		System.out.println("Answer to part 1: " + new Day02().runPart1(lines));
+		System.out.println("Took: " + (Calendar.getInstance().getTimeInMillis() - start) + " ms");
+		start = Calendar.getInstance().getTimeInMillis();
+		System.out.println("Answer to part 1: " + new Day02().runPart2(lines));
+		System.out.println("Took: " + (Calendar.getInstance().getTimeInMillis() - start) + " ms");
+
+	}
+
+}

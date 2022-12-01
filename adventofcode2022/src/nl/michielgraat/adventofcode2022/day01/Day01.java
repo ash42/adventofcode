53259b9408d216e4d0370acce7c181fa17bc2ca7
Michiel Graat <github@michielgraat.nl>
2022-12-01 11:10:23 +0100

    Day 1

--- adventofcode2022/src/nl/michielgraat/adventofcode2022/day01/Day01.java
+++ adventofcode2022/src/nl/michielgraat/adventofcode2022/day01/Day01.java
@@ -1,6 +1,10 @@
 package nl.michielgraat.adventofcode2022.day01;
 
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import nl.michielgraat.adventofcode2022.AocSolver;
 
@@ -10,36 +14,42 @@ public class Day01 extends AocSolver {
         super(filename);
     }
 
-    private int getResult(final List<String> input, final boolean part1) {
-        int one = 0;
-        int two = 0;
-        int three = 0;
+    @Override
+    protected String runPart2(final List<String> input) {
+        final Map<Integer, Integer> elfToCal = new HashMap<>();
+        int currentElf = 1;
         int currentTotal = 0;
         for (int i = 0; i < input.size(); i++) {
             final String line = input.get(i);
             if (line.isEmpty() || i == input.size() - 1) {
-                if (currentTotal > one) {
-                    three = two;
-                    two = one;
-                    one = currentTotal;
+                elfToCal.put(currentElf, currentTotal);
+                currentElf++;
+                currentTotal = 0;
+            } else {
+                currentTotal += Integer.parseInt(line);
+            }
+        }
+        return String.valueOf(elfToCal.values().stream().sorted(Comparator.reverseOrder()).limit(3)
+                .mapToInt(Integer::intValue).sum());
+
+    }
+
+    @Override
+    protected String runPart1(final List<String> input) {
+        int max = 0;
+        int currentTotal = 0;
+        for (int i = 0; i < input.size(); i++) {
+            final String line = input.get(i);
+            if (line.isEmpty() || i == input.size() - 1) {
+                if (currentTotal > max) {
+                    max = currentTotal;
                 }
                 currentTotal = 0;
             } else {
                 currentTotal += Integer.parseInt(line);
             }
         }
-        return part1 ? one : one + two + three;
-    }
-
-    @Override
-    protected String runPart2(final List<String> input) {
-        return String.valueOf(getResult(input, false));
-
-    }
-
-    @Override
-    protected String runPart1(final List<String> input) {
-        return String.valueOf(getResult(input, true));
+        return String.valueOf(max);
     }
 
     public static void main(final String... args) {

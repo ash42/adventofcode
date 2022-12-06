c688f6ca65479f5dbcefeb7d34e9f2b0ea159ca9
Michiel Graat <github@michielgraat.nl>
2022-12-06 09:00:40 +0100

    Day 6

---
+++ adventofcode2022/src/nl/michielgraat/adventofcode2022/day06/Day06.java
@@ -0,0 +1,45 @@
+package nl.michielgraat.adventofcode2022.day06;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import nl.michielgraat.adventofcode2022.AocSolver;
+
+public class Day06 extends AocSolver {
+
+    private static final int PACKET_MARKER_SIZE = 4;
+    private static final int MESSAGE_MARKER_SIZE = 14;
+
+    protected Day06(final String filename) {
+        super(filename);
+    }
+
+    private int getMarkerPosition(final String buffer, final boolean part1) {
+        final int markerSize = part1 ? PACKET_MARKER_SIZE : MESSAGE_MARKER_SIZE;
+        for (int i = 0; i < buffer.length() - markerSize; i++) {
+            final Set<Character> letters = new HashSet<>();
+            for (int j = i; j < i + markerSize; j++) {
+                letters.add(buffer.charAt(j));
+            }
+            if (letters.size() == markerSize) {
+                return i + markerSize;
+            }
+        }
+        throw new IllegalArgumentException("No marker found");
+    }
+
+    @Override
+    protected String runPart2(final List<String> input) {
+        return String.valueOf(getMarkerPosition(input.get(0), false));
+    }
+
+    @Override
+    protected String runPart1(final List<String> input) {
+        return String.valueOf(getMarkerPosition(input.get(0), true));
+    }
+
+    public static void main(final String... args) {
+        new Day06("day06.txt");
+    }
+}

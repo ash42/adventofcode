3eb953e625103881ba53ad4dc919df81f0e6acb2
Michiel Graat <github@michielgraat.nl>
2022-12-20 16:22:41 +0100

    Day 18

--- adventofcode2022/src/nl/michielgraat/adventofcode2022/day18/Point3d.java
+++ adventofcode2022/src/nl/michielgraat/adventofcode2022/day18/Point3d.java
@@ -5,23 +5,23 @@ import java.util.List;
 
 public record Point3d(int x, int y, int z) implements Comparable<Point3d> {
 
-    public boolean adjacent(Point3d o) {
+    public boolean adjacent(final Point3d o) {
         return Math.abs(this.x - o.x) + Math.abs(this.y - o.y) + Math.abs(this.z - o.z) == 1;
     }
 
     public List<Point3d> getNeighbours() {
-        List<Point3d> neighbours = new ArrayList<>();
-        neighbours.add(new Point3d(x+1,y,z));
-        neighbours.add(new Point3d(x-1,y,z));
-        neighbours.add(new Point3d(x,y+1,z));
-        neighbours.add(new Point3d(x,y-1,z));
-        neighbours.add(new Point3d(x,y,z+1));
-        neighbours.add(new Point3d(x,y,z-1));
+        final List<Point3d> neighbours = new ArrayList<>();
+        neighbours.add(new Point3d(x + 1, y, z));
+        neighbours.add(new Point3d(x - 1, y, z));
+        neighbours.add(new Point3d(x, y + 1, z));
+        neighbours.add(new Point3d(x, y - 1, z));
+        neighbours.add(new Point3d(x, y, z + 1));
+        neighbours.add(new Point3d(x, y, z - 1));
         return neighbours;
     }
 
     @Override
-    public int compareTo(Point3d o) {
+    public int compareTo(final Point3d o) {
         if (this.z != o.z) {
             return Integer.compare(this.z, o.z);
         } else if (this.y != o.y) {

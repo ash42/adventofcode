78f40bf7793d4445854e20c969ae179d28465b87
Michiel Graat <michiel.graat@sidn.nl>
2023-12-14 16:16:06 +0100

    Day 14

---
+++ adventofcode2023/src/nl/michielgraat/adventofcode2023/day14/Day14.java
@@ -0,0 +1,174 @@
+package nl.michielgraat.adventofcode2023.day14;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import nl.michielgraat.adventofcode2023.AocSolver;
+
+public class Day14 extends AocSolver {
+
+    protected Day14(String filename) {
+        super(filename);
+    }
+
+    private List<Rock> readRocks(final List<String> input) {
+        List<Rock> rocks = new ArrayList<>();
+
+        for (int y = 0; y < input.size(); y++) {
+            String row = input.get(y);
+            for (int x = 0; x < row.length(); x++) {
+                char type = row.charAt(x);
+                if (type == 'O' || type == '#') {
+                    rocks.add(new Rock(x, y, type));
+                }
+            }
+        }
+        return rocks;
+    }
+
+    private List<Rock> tiltNorth(List<Rock> rocks) {
+        Collections.sort(rocks);
+        List<Rock> result = new ArrayList<>();
+
+        for (Rock rock : rocks) {
+            int max = result.stream().filter(r -> r.x() == rock.x()).mapToInt(Rock::y).max().orElse(-1);
+            if (rock.type() == 'O') {
+                result.add(new Rock(rock.x(), rock.y() == 0 ? 0 : max + 1, rock.type()));
+            } else {
+                result.add(rock);
+            }
+        }
+
+        return result;
+    }
+
+    private List<Rock> tiltEast(List<Rock> rocks, int maxX) {
+        Collections.sort(rocks);
+        Collections.reverse(rocks);
+        List<Rock> result = new ArrayList<>();
+
+        for (Rock rock : rocks) {
+            int min = result.stream().filter(r -> r.y() == rock.y()).mapToInt(Rock::x).min().orElse(maxX + 1);
+            if (rock.type() == 'O') {
+                Rock r = new Rock(rock.x() == maxX ? maxX : min - 1, rock.y(), rock.type());
+                result.add(r);
+            } else {
+                result.add(rock);
+            }
+        }
+
+        return result;
+    }
+
+    private List<Rock> tiltSouth(List<Rock> rocks, int maxY) {
+        Collections.sort(rocks);
+        Collections.reverse(rocks);
+        List<Rock> result = new ArrayList<>();
+
+        for (Rock rock : rocks) {
+            int min = result.stream().filter(r -> r.x() == rock.x()).mapToInt(Rock::y).min().orElse(maxY);
+            if (rock.type() == 'O') {
+                Rock r = new Rock(rock.x(), rock.y() == maxY ? maxY : min - 1, rock.type());
+                result.add(r);
+            } else {
+                result.add(rock);
+            }
+        }
+        return result;
+    }
+
+    private List<Rock> tiltWest(List<Rock> rocks) {
+        Collections.sort(rocks);
+        List<Rock> result = new ArrayList<>();
+
+        for (Rock rock : rocks) {
+            int max = result.stream().filter(r -> r.y() == rock.y()).mapToInt(Rock::x).max().orElse(-1);
+            if (rock.type() == 'O') {
+                Rock r = new Rock(rock.x() == 0 ? 0 : max + 1, rock.y(), rock.type());
+                result.add(r);
+            } else {
+                result.add(rock);
+            }
+        }
+
+        return result;
+    }
+
+    private List<Rock> cycle(List<Rock> rocks, int maxX, int maxY) {
+        return tiltEast(tiltSouth(tiltWest(tiltNorth(rocks)), maxY), maxX);
+    }
+
+    private int calculateWeight(List<Rock> rocks, int size) {
+        return rocks.stream().filter(r -> r.type() == 'O').mapToInt(r -> size - r.y()).sum();
+    }
+
+    private List<Integer> findPattern(final List<Integer> values) {
+        int slowIdx = 0;
+        int slow = values.get(slowIdx);
+        int fastIdx = 0;
+        int fast;
+        boolean met = false;
+        while (fastIdx + 5 < values.size()) {
+            slowIdx++;
+            slow = values.get(slowIdx);
+            fastIdx += 2;
+            fast = values.get(fastIdx);
+            if (slow == fast) {
+                met = true;
+                break;
+            }
+        }
+        if (!met) {
+            throw new IllegalArgumentException("No cycle found");
+        }
+        final int startIdx = slowIdx;
+        slowIdx++;
+        int slow2 = values.get(slowIdx);
+        while (slow2 != slow) {
+            slowIdx++;
+            slow2 = values.get(slowIdx);
+        }
+        final List<Integer> pattern = new ArrayList<>();
+        for (int i = startIdx; i < slowIdx; i++) {
+            pattern.add(values.get(i));
+        }
+        return pattern;
+    }
+
+    private List<Integer> getFirstNWeights(int n, List<String> input) {
+        List<Rock> rocks = readRocks(input);
+        List<Integer> weights = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            rocks = cycle(rocks, input.get(0).length() - 1, input.size());
+            weights.add(calculateWeight(rocks, input.size()));
+        }
+        return weights;
+    }
+
+    private int getValueFor(int n, List<Integer> weights) {
+        List<Integer> pattern = findPattern(weights);
+
+        int valToFetch = (n % pattern.size()) - 1;
+        if (valToFetch < 0) {
+            valToFetch = pattern.size() - 1;
+        }
+        return pattern.get(valToFetch);
+    }
+
+    @Override
+    protected String runPart2(final List<String> input) {
+        List<Integer> weights = getFirstNWeights(300, input);
+        return String.valueOf(getValueFor(1000000000, weights));
+    }
+
+    @Override
+    protected String runPart1(final List<String> input) {
+        List<Rock> rocks = tiltNorth(readRocks(input));
+        return String.valueOf(calculateWeight(rocks, input.size()));
+    }
+
+    public static void main(String... args) {
+        new Day14("day14.txt");
+    }
+}
---
+++ adventofcode2023/src/nl/michielgraat/adventofcode2023/day14/Rock.java
@@ -0,0 +1,38 @@
+package nl.michielgraat.adventofcode2023.day14;
+
+public record Rock (int x, int y, char type) implements Comparable<Rock> {
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + x;
+        result = prime * result + y;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Rock other = (Rock) obj;
+        if (x != other.x)
+            return false;
+        if (y != other.y)
+            return false;
+        return true;
+    }
+
+    @Override
+    public int compareTo(Rock o) {
+        if (this.y != o.y()) {
+            return Integer.compare(this.y, o.y());
+        } else {
+            return Integer.compare(this.x, o.x());
+        }
+    }
+}

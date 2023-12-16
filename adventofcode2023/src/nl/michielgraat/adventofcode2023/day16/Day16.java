7ec3c771698bd54952831d979b0d8875be28841d
Michiel Graat <michiel.graat@sidn.nl>
2023-12-16 14:28:35 +0100

    Day 16

--- adventofcode2023/src/nl/michielgraat/adventofcode2023/day16/Day16.java
+++ adventofcode2023/src/nl/michielgraat/adventofcode2023/day16/Day16.java
@@ -42,7 +42,7 @@ public class Day16 extends AocSolver {
      * @param y           The y to start at.
      * @param grid        The grid we are walking.
      * @param direction   The direction we are going in.
-     * @param loopList    A list which contains all pair of (x,y) coordinate &
+     * @param loopSet     A set which contains all pair of (x,y) coordinate &
      *                    direction. This is used for loop detection: if we are at
      *                    an (x,y) and going in a direction which we have seen
      *                    before, we stop (return 0).
@@ -51,13 +51,13 @@ public class Day16 extends AocSolver {
      *                    once.
      * @return The nr of energized tiles (with duplicates)
      */
-    private int getNrEnergizedTiles(int x, int y, char[][] grid, int direction, List<LoopDetectionKey> loopList,
+    private int getNrEnergizedTiles(int x, int y, char[][] grid, int direction, Set<LoopDetectionKey> loopSet,
             List<Coordinate> visitedList) {
         // Make sure we haven't visited this space before while going the same
         // direction, if so, we are in a loop, so stop.
         LoopDetectionKey key = new LoopDetectionKey(x, y, direction);
         Coordinate c = new Coordinate(x, y);
-        if (x < 0 || y < 0 || y >= grid.length || x >= grid[y].length || loopList.contains(key)) {
+        if (x < 0 || y < 0 || y >= grid.length || x >= grid[y].length || loopSet.contains(key)) {
             memoMap.put(key, 0);
             return 0;
         }
@@ -66,42 +66,42 @@ public class Day16 extends AocSolver {
             return memoMap.get(key);
         }
 
-        loopList.add(key);
+        loopSet.add(key);
         visitedList.add(c);
 
         if (grid[y][x] == '.' || (grid[y][x] == '|' && (direction == UP || direction == DOWN))
                 || (grid[y][x] == '-' && (direction == LEFT || direction == RIGHT))) {
             int newX = (direction == UP || direction == DOWN) ? x : (direction == LEFT) ? x - 1 : x + 1;
             int newY = (direction == LEFT || direction == RIGHT) ? y : (direction == UP) ? y - 1 : y + 1;
-            int nr = 1 + getNrEnergizedTiles(newX, newY, grid, direction, loopList, visitedList);
+            int nr = 1 + getNrEnergizedTiles(newX, newY, grid, direction, loopSet, visitedList);
             memoMap.put(key, nr);
             return nr;
         } else if (grid[y][x] == '/') {
             int newX = (direction == LEFT || direction == RIGHT) ? x : (direction == UP) ? x + 1 : x - 1;
             int newY = (direction == UP || direction == DOWN) ? y : (direction == LEFT) ? y + 1 : y - 1;
             int newDirection = direction == LEFT ? DOWN : direction == RIGHT ? UP : direction == UP ? RIGHT : LEFT;
-            int nr = 1 + getNrEnergizedTiles(newX, newY, grid, newDirection, loopList, visitedList);
+            int nr = 1 + getNrEnergizedTiles(newX, newY, grid, newDirection, loopSet, visitedList);
             memoMap.put(key, nr);
             return nr;
         } else if (grid[y][x] == '\\') {
             int newX = (direction == LEFT || direction == RIGHT) ? x : (direction == UP) ? x - 1 : x + 1;
             int newY = (direction == UP || direction == DOWN) ? y : (direction == LEFT) ? y - 1 : y + 1;
             int newDirection = direction == LEFT ? UP : direction == RIGHT ? DOWN : direction == UP ? LEFT : RIGHT;
-            int nr = 1 + getNrEnergizedTiles(newX, newY, grid, newDirection, loopList, visitedList);
+            int nr = 1 + getNrEnergizedTiles(newX, newY, grid, newDirection, loopSet, visitedList);
             memoMap.put(key, nr);
             return nr;
         } else if (grid[y][x] == '-') {
             // Direction should always be up or down, since the other cases have already
             // been covered by the first if.
-            int nr = 1 + getNrEnergizedTiles(x - 1, y, grid, LEFT, loopList, visitedList)
-                    + getNrEnergizedTiles(x + 1, y, grid, RIGHT, loopList, visitedList);
+            int nr = 1 + getNrEnergizedTiles(x - 1, y, grid, LEFT, loopSet, visitedList)
+                    + getNrEnergizedTiles(x + 1, y, grid, RIGHT, loopSet, visitedList);
             memoMap.put(key, nr);
             return nr;
         } else {
             // Current space is |. Direction should always be left or right, since the other
             // cases have already been covered by the first if.
-            int nr = 1 + getNrEnergizedTiles(x, y - 1, grid, UP, loopList, visitedList)
-                    + getNrEnergizedTiles(x, y + 1, grid, DOWN, loopList, visitedList);
+            int nr = 1 + getNrEnergizedTiles(x, y - 1, grid, UP, loopSet, visitedList)
+                    + getNrEnergizedTiles(x, y + 1, grid, DOWN, loopSet, visitedList);
             memoMap.put(key, nr);
             return nr;
         }
@@ -110,7 +110,7 @@ public class Day16 extends AocSolver {
     private int getNrUniqueEnergizedTiles(int x, int y, int direction, char[][] grid) {
         List<Coordinate> visited = new ArrayList<>();
         memoMap = new HashMap<>();
-        int total = getNrEnergizedTiles(x, y, grid, direction, new ArrayList<>(), visited);
+        int total = getNrEnergizedTiles(x, y, grid, direction, new HashSet<>(), visited);
         Set<Coordinate> unique = new HashSet<>(visited);
         return total - (visited.size() - unique.size());
     }

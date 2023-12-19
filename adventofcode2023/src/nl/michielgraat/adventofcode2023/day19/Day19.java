9efd0e5b0e60f62daf263bdac1b3d7087389150b
Michiel Graat <michiel.graat@sidn.nl>
2023-12-19 15:30:41 +0100

    Day 19

---
+++ adventofcode2023/src/nl/michielgraat/adventofcode2023/day19/Day19.java
@@ -0,0 +1,26 @@
+package nl.michielgraat.adventofcode2023.day19;
+
+import java.util.List;
+
+import nl.michielgraat.adventofcode2023.AocSolver;
+
+public class Day19 extends AocSolver {
+
+    protected Day19(String filename) {
+        super(filename);
+    }
+
+    @Override
+    protected String runPart2(final List<String> input) {
+        return "part 2";
+    }
+
+    @Override
+    protected String runPart1(final List<String> input) {
+        return "part 1";
+    }
+    
+    public static void main(String... args) {
+        new Day19("day19.txt");
+    }
+}

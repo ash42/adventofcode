a7764ae1029fec14dc8464ee2be8adb5b99a3fc2
Michiel Graat <michiel.graat@sidn.nl>
2023-12-24 14:15:20 +0100

    Day 24

---
+++ adventofcode2023/src/nl/michielgraat/adventofcode2023/day24/Day24.java
@@ -0,0 +1,26 @@
+package nl.michielgraat.adventofcode2023.day24;
+
+import java.util.List;
+
+import nl.michielgraat.adventofcode2023.AocSolver;
+
+public class Day24 extends AocSolver {
+
+    protected Day24(String filename) {
+        super(filename);
+    }
+
+    @Override
+    protected String runPart2(final List<String> input) {
+        return "part 2";
+    }
+
+    @Override
+    protected String runPart1(final List<String> input) {
+        return "part 1";
+    }
+    
+    public static void main(String... args) {
+        new Day24("day24.txt");
+    }
+}

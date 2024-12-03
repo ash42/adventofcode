64e9aa3d040655c0b7d421f68ea73cb4c6fa75a1
Michiel Graat <michiel.graat@sidn.nl>
2024-12-03 17:51:07 +0100

    Day 3

---
+++ adventofcode2024/src/nl/michielgraat/adventofcode2024/day03/Day03.java
@@ -0,0 +1,84 @@
+package nl.michielgraat.adventofcode2024.day03;
+
+import java.util.List;
+import java.util.TreeMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import nl.michielgraat.adventofcode2024.AocSolver;
+
+public class Day03 extends AocSolver {
+
+    protected Day03(final String filename) {
+        super(filename);
+    }
+
+    private String getMemory(final List<String> input) {
+        final StringBuilder sb = new StringBuilder();
+        for (final String line : input) {
+            sb.append(line);
+        }
+        return sb.toString();
+    }
+
+    private TreeMap<Integer, Boolean> getSortedDoDontIndexes(final String memory) {
+        final Matcher m = Pattern.compile("do\\(\\)").matcher(memory);
+        final Matcher m2 = Pattern.compile("don\\'t\\(\\)").matcher(memory);
+
+        final TreeMap<Integer, Boolean> indexes = new TreeMap<>();
+        while (m.find()) {
+            indexes.put(m.start(), true);
+        }
+        while (m2.find()) {
+            indexes.put(m2.start(), false);
+        }
+        return indexes;
+    }
+
+    private int getTotal(final String memory) {
+        final TreeMap<Integer, Boolean> multIndexes = getSortedDoDontIndexes(memory);
+        int total = 0;
+        final Matcher matcher = Pattern.compile("mul\\(\\d{1,3},\\d{1,3}\\)").matcher(memory);
+
+        while (matcher.find()) {
+            final int index = matcher.start();
+            final boolean multiply = multIndexes.floorKey(index) != null ? multIndexes.get(multIndexes.floorKey(index))
+                    : true;
+            String match = matcher.group();
+            if (multiply) {
+                match = match.substring(4);
+                match = match.substring(0, match.length() - 1);
+                final int val1 = Integer.parseInt(match.split(",")[0]);
+                final int val2 = Integer.parseInt(match.split(",")[1]);
+                total += val1 * val2;
+            }
+        }
+        return total;
+    }
+
+    @Override
+    protected String runPart2(final List<String> input) {
+        final String memory = getMemory(input);
+        return String.valueOf(getTotal(memory));
+    }
+
+    @Override
+    protected String runPart1(final List<String> input) {
+        int total = 0;
+        for (final String line : input) {
+            final Matcher matcher = Pattern.compile("mul\\(\\d{1,3},\\d{1,3}\\)").matcher(line);
+            while (matcher.find()) {
+                String match = matcher.group().substring(4);
+                match = match.substring(0, match.length() - 1);
+                final int val1 = Integer.parseInt(match.split(",")[0]);
+                final int val2 = Integer.parseInt(match.split(",")[1]);
+                total += val1 * val2;
+            }
+        }
+        return String.valueOf(total);
+    }
+
+    public static void main(final String... args) {
+        new Day03("day03.txt");
+    }
+}

761ea6b45c29f5ced94f70459d29b45506013ad1
Michiel Graat <michiel.graat@sidn.nl>
2025-12-09 23:33:45 +0100

    Day 9

--- adventofcode2025/src/nl/michielgraat/adventofcode2025/day09/Rectangle.java
+++ adventofcode2025/src/nl/michielgraat/adventofcode2025/day09/Rectangle.java
@@ -19,6 +19,27 @@ public record Rectangle(RedTile r1, RedTile r2) {
             final long eMaxX = Math.max(e.r1().x(), e.r2().x());
             final long eMinY = Math.min(e.r1().y(), e.r2().y());
             final long eMaxY = Math.max(e.r1().y(), e.r2().y());
+            // The idea is that two rectangles collide if they intersect on both axes.
+            // In this case we are checking if the rectangle collides with one of the edges
+            // of the polygon.
+            //
+            // So what we are checking here if the rectangle is above, below, left or right
+            // of the edge.
+            //
+            // Since we are talking about comparing an edge with a rectangle here, this also
+            // works if the rectangle is completely inside of the polygon (because then it
+            // will not intersect with any of the edges).
+            //
+            // Note that a rectangle completely outside the polygon also does not intersect
+            // with any of the edges. For most cases this is no problem (as all rectangles
+            // we generate have corners which are red tile), but for some cases (like the
+            // rectangle defined by (2,5) to (9,7) in the test input) this gives incorrect
+            // results. Because of the way the input is constructed this does not lead to
+            // problems for the actual answer.
+            //
+            // This also works (the rectangle does not intersect with an edge) if one of the
+            // edges of the polygon also happens to be the edge of the rectangle (as we are
+            // using < and >, and not <= and =>).
             if (minX < eMaxX && maxX > eMinX && minY < eMaxY && maxY > eMinY) {
                 return true;
             }

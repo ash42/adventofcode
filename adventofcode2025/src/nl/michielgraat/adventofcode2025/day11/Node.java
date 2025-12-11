98781339e218a959c8f08cede068add574d3a65c
Michiel Graat <michiel.graat@sidn.nl>
2025-12-11 22:34:06 +0100

    Day 11

---
+++ adventofcode2025/src/nl/michielgraat/adventofcode2025/day11/Node.java
@@ -0,0 +1,7 @@
+package nl.michielgraat.adventofcode2025.day11;
+
+import java.util.List;
+
+public record Node(String current, List<String> path) {
+
+}
---
+++ adventofcode2025/src/nl/michielgraat/adventofcode2025/day11/QueueNode.java
@@ -0,0 +1,5 @@
+package nl.michielgraat.adventofcode2025.day11;
+
+public record QueueNode(String device, int nrSteps) {
+
+}

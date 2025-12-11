66e33a5fd4fbfc61b6e741dde5698870ea028b4c
Michiel Graat <michiel.graat@sidn.nl>
2025-12-11 22:51:21 +0100

    Day 11

--- adventofcode2025/src/nl/michielgraat/adventofcode2025/day11/Day11.java
+++ adventofcode2025/src/nl/michielgraat/adventofcode2025/day11/Day11.java
@@ -1,13 +1,9 @@
 package nl.michielgraat.adventofcode2025.day11;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Queue;
-
 import nl.michielgraat.adventofcode2025.AocSolver;
 
 public class Day11 extends AocSolver {
@@ -26,79 +22,10 @@ public class Day11 extends AocSolver {
     }
 
     /**
-     * Counts the number of steps from the first item added to the queue and the
-     * finish.
-     */
-    private int getNrSteps(final String finish, final Queue<QueueNode> queue, final Map<String, List<String>> devices) {
-        final QueueNode current = queue.poll();
-        if (!finish.equals("out") && current.device().equals("out")) {
-            // Extra piece of logic for the case where "out" is not the finish (because the
-            // devices map does not contain an "out" entry).
-            return 0;
-        }
-        if (current.device().equals(finish)) {
-            return current.nrSteps();
-        } else {
-            for (final String device : devices.get(current.device())) {
-                final QueueNode node = new QueueNode(device, current.nrSteps() + 1);
-                if (!queue.contains(node)) {
-                    queue.add(node);
-                }
-            }
-            return getNrSteps(finish, queue, devices);
-        }
-    }
-
-    /**
-     * For a given node, calculate all the possible from which it can be reached.
-     */
-    private List<String> buildPossiblePathMembers(final String finish, final Map<String, List<String>> devices) {
-        final List<String> result = new ArrayList<>();
-        result.add(finish);
-        for (final String node : devices.keySet()) {
-            if (!node.equals(finish)) {
-                final Queue<QueueNode> queue = new LinkedList<>();
-                queue.add(new QueueNode(node, 0));
-                if (getNrSteps(finish, queue, devices) > 0) {
-                    result.add(node);
-                }
-            }
-        }
-        return result;
-    }
-
-    /**
-     * Calculates all paths between two nodes.
-     */
-    private void getAllPaths(final String current, final String finish, final List<String> path,
-            final List<List<String>> result,
-            final List<String> visited, final List<String> neighbours, final Map<String, List<String>> devices) {
-        if (!finish.equals("out") && current.equals("out")) {
-            // Extra piece of logic for when the finish is not "out".
-            return;
-        }
-        if (current.equals(finish)) {
-            // Found a path to the finish, add it to the result list.
-            result.add(path.stream().toList());
-        } else {
-            for (final String neighbour : devices.get(current)) {
-                // Check if the finish is reachable for the current neighbour (for pruning of
-                // the search tree) and if it has not been visited yet.
-                if (neighbours.contains(neighbour) && !visited.contains(neighbour)) {
-                    visited.add(neighbour);
-                    path.add(neighbour);
-                    getAllPaths(neighbour, finish, path, result, visited, neighbours, devices);
-                    visited.remove(neighbour);
-                    path.removeLast();
-                }
-            }
-        }
-    }
-
-    /**
      * Counts the number of path between any two nodes.
      */
-    private long countNrPaths(final String current, final String finish, final Map<String, List<String>> devices) {
+    private long countNrPaths(final String current, final String finish,
+            final Map<String, List<String>> devices, final Map<String, Long> memo) {
         if (!finish.equals("out") && current.equals("out")) {
             // If the finish is not defined as "out", we have this extra piece of logic,
             // because "out" does not have an entry in the devices map.
@@ -106,39 +33,33 @@ public class Day11 extends AocSolver {
         } else if (current.equals(finish)) {
             return 1;
         } else {
+            if (memo.containsKey(current)) {
+                return memo.get(current);
+            }
             long total = 0;
             for (final String output : devices.get(current)) {
-                total += countNrPaths(output, finish, devices);
+                total += countNrPaths(output, finish, devices, memo);
+
             }
+            memo.put(current, total);
             return total;
         }
-    }
-
-    private long getNrPathsBetween(final String start, final String finish, final Map<String, List<String>> devices) {
-        final List<String> neighbours = buildPossiblePathMembers(finish, devices);
-        final List<List<String>> result = new ArrayList<>();
-        final List<String> path = new ArrayList<>();
-        path.add(start);
-        final List<String> visited = new ArrayList<>();
-        visited.add(start);
-        getAllPaths(start, finish, path, result, visited, neighbours, devices);
-        return result.size();
     }
 
     @Override
     protected String runPart2(final List<String> input) {
         final Map<String, List<String>> devices = parseDevices(input);
 
-        final long fftPaths = getNrPathsBetween("svr", "fft", devices);
-        final long dacPaths = getNrPathsBetween("fft", "dac", devices);
-        final long outPaths = getNrPathsBetween("dac", "out", devices);
+        final long fftPaths = countNrPaths("svr", "fft", devices, new HashMap<>());
+        final long dacPaths = countNrPaths("fft", "dac", devices, new HashMap<>());
+        final long outPaths = countNrPaths("dac", "out", devices, new HashMap<>());
 
         return String.valueOf(fftPaths * dacPaths * outPaths);
     }
 
     @Override
     protected String runPart1(final List<String> input) {
-        return String.valueOf(countNrPaths("you", "out", parseDevices(input)));
+        return String.valueOf(countNrPaths("you", "out", parseDevices(input), new HashMap<>()));
     }
 
     public static void main(final String... args) {
--- adventofcode2025/src/nl/michielgraat/adventofcode2025/day11/Node.java
+++
@@ -1,7 +0,0 @@
-package nl.michielgraat.adventofcode2025.day11;
-
-import java.util.List;
-
-public record Node(String current, List<String> path) {
-
-}
